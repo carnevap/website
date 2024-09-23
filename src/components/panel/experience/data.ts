7c05c2c9feb9c4595ae294862ad51e63b660c87c
Patrick Carnevale <90724463+carnevap@users.noreply.github.com>
2024-09-23 18:10:04 -0400

    Update data.ts

--- src/components/panel/experience/data.ts
+++ src/components/panel/experience/data.ts
@@ -12,11 +12,11 @@ export interface IExperience {
 
 export const experienceData: IExperience[] = [
   {
-    title: 'Software Engineer',
-    companyName: 'Google',
-    companyImage: image1,
-    date: 'Jan 2024 - Aug 2024',
-    points: ['Point 1', 'Point 2', 'Point 3', 'Point 4', 'Point 5'],
+    title: 'IT Infrastructure Support Analyst',
+    companyName: 'CRA',
+    companyImage: CRA.jpg,
+    date: 'Sep 2022 - Aug 2023 (1 year)',
+    points: ['Provided technical support and maintained IT infrastructure to ensure smooth operations', 'Collaborated with IT teams to escalate and resolve complex technical issues efficiently', 'Implemented monitoring tools and procedures to identify potential issues proactively and mitigate risks', 'Ensured system stability and reliability through continuous infrastructure maintenance', 'Facilitated communication between IT teams and end-users to ensure timely issue resolution'],
   },
 
   {
