c5e422175780f660a8d2cadfb459ab5540e5a604
Patrick Carnevale <90724463+carnevap@users.noreply.github.com>
2024-09-24 17:58:26 -0400

    Update data.ts

--- src/components/panel/experience/data.ts
+++ src/components/panel/experience/data.ts
@@ -21,8 +21,8 @@ export const experienceData: IExperience[] = [
 
   {
     title: 'Sr Multimedia Intern',
-    companyName: 'Canada Revenue Agency',
-    companyImage: image1,
+    companyName: 'University of Windsory',
+    companyImage: image2,
     date: 'Sep 2022 - Aug 2023 (1 year)',
     points: ['Provided technical support and maintained IT infrastructure to ensure smooth operations', 'Collaborated with IT teams to escalate and resolve complex technical issues efficiently', 'Implemented monitoring tools and procedures to identify potential issues proactively and mitigate risks', 'Ensured system stability and reliability through continuous infrastructure maintenance', 'Facilitated communication between IT teams and end-users to ensure timely issue resolution'],
   },
