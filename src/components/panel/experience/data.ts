1effdab07f1c7ec16332adba5c37bac1609821cd
Patrick Carnevale <90724463+carnevap@users.noreply.github.com>
2024-09-24 18:21:08 -0400

    Update data.ts

--- src/components/panel/experience/data.ts
+++ src/components/panel/experience/data.ts
@@ -23,7 +23,7 @@ export const experienceData: IExperience[] = [
     title: 'Sr Multimedia Intern',
     companyName: 'University of Windsory',
     companyImage: image2,
-    date: 'Sep 2022 - Aug 2023 (1 year)',
-    points: ['Provided technical support and maintained IT infrastructure to ensure smooth operations', 'Collaborated with IT teams to escalate and resolve complex technical issues efficiently', 'Implemented monitoring tools and procedures to identify potential issues proactively and mitigate risks', 'Ensured system stability and reliability through continuous infrastructure maintenance', 'Facilitated communication between IT teams and end-users to ensure timely issue resolution'],
+    date: 'May 2021 - Apr 2022 (1 year)',
+    points: ['Developed learning resources, including videos, graphics, websites, and animations', 'Designed visual elements for student and faculty educational content', 'Collaborated with departments and schools on multimedia projects', 'Enhanced the university's multimedia presence through diverse content', 'Supported faculty and students with engaging digital resources'],
   },
 ];
