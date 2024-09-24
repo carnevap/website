5a5b06c9518102c25563de6b3303be1c85cad3a0
Patrick Carnevale <90724463+carnevap@users.noreply.github.com>
2024-09-24 17:50:56 -0400

    Update data.ts

--- src/components/panel/experience/data.ts
+++ src/components/panel/experience/data.ts
@@ -18,12 +18,4 @@ export const experienceData: IExperience[] = [
     date: 'Sep 2022 - Aug 2023 (1 year)',
     points: ['Provided technical support and maintained IT infrastructure to ensure smooth operations', 'Collaborated with IT teams to escalate and resolve complex technical issues efficiently', 'Implemented monitoring tools and procedures to identify potential issues proactively and mitigate risks', 'Ensured system stability and reliability through continuous infrastructure maintenance', 'Facilitated communication between IT teams and end-users to ensure timely issue resolution'],
   },
-
-  {
-    title: 'IT Infrastructure Support Analyst',
-    companyName: 'University of Windsor',
-    companyImage: image2,
-    date: 'May 2021 - Apr 2022 (1 year)',
-    points: ['Developed learning resources, including videos, graphics, websites, and animations', 'Designed visual elements for student and faculty educational content', 'Collaborated with departments and schools on multimedia projects', 'Enhanced the university's multimedia presence through diverse content', 'Supported faculty and students with engaging digital resources'],
-  },
 ];
