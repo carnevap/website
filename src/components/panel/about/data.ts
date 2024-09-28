13f4a1ba2851695b84f8cc1ab7bada74a98a6be8
Patrick Carnevale <90724463+carnevap@users.noreply.github.com>
2024-09-27 20:31:43 -0400

    Update data.ts

--- src/components/panel/about/data.ts
+++ src/components/panel/about/data.ts
@@ -14,5 +14,5 @@ export const aboutData: IAbout = {
     { icon: faLinkedin, url: 'https://www.linkedin.com/in/patrick-carnevale/' },
     { icon: faYoutube, url: 'https://www.youtube.com/novipit' },
   ],
-  description: `Hi, I'm Patrick Carnevale a computer science graduate. Don't hesitate to reach out through LinkedIn if you'd like to connect.`,
+  description: `Hey! I’m Patrick Carnevale, a bilingual (English/French) Computer Science graduate with experience in IT support, multimedia, and software development. I’m skilled in Python, Java, C++, and more, and have worked on projects in AI, 3D modeling, and game design. I thrive in team environments, excel at problem-solving, and adapt quickly to new challenges!`,
 };
