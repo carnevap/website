542a449460d8c485d69f63541ad061786355e359
Patrick Carnevale <90724463+carnevap@users.noreply.github.com>
2024-09-30 10:45:36 -0400

    Update data.ts

--- src/components/panel/about/data.ts
+++ src/components/panel/about/data.ts
@@ -12,7 +12,7 @@ export const aboutData: IAbout = {
   links: [
     { icon: faGithub, url: 'https://github.com/carnevap' },
     { icon: faLinkedin, url: 'https://www.linkedin.com/in/patrick-carnevale/' },
-    { icon: faYoutube, url: 'https://www.youtube.com/novipit' },
+    { icon: faYoutube, url: 'https://www.youtube.com/@DevForgeCoding' },
   ],
   description: `Hey! I’m Patrick Carnevale, a bilingual (English/French) Computer Science graduate with experience in IT support, multimedia, and software development. I’m skilled in Python, Java, C++, and more, and have worked on projects in AI, 3D modeling, and game design. I thrive in team environments, excel at problem-solving, and adapt quickly to new challenges!`,
 };
