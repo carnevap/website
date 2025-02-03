2e02d1a01d3961bd0fb6975478fffcd30d936475
Patrick Carnevale <90724463+carnevap@users.noreply.github.com>
2025-02-03 14:06:24 -0500

    Update data.ts

--- src/components/panel/experience/data.ts
+++ src/components/panel/experience/data.ts
@@ -14,11 +14,11 @@ export interface IExperience {
 
 export const experienceData: IExperience[] = [
   {
-    title: 'Bilingual Support Representative',
+    title: 'Bilingual Customer Support Representative',
     companyName: 'Toyota Canada',
     companyImage: image3,
     date: 'Jul 2024 - Present',
-    points: ['Utilized AWS cloud services to aid Toyota Canada clients', 'Provided clients with information and technical support', 'Promoted multiple times in quick succession after only a few months of working', 'Handle high priority emails and sensitive informations as well as address in-app feedback issues', 'Made use of my bilingualism to support both English and French clients'],
+    points: ['Handled phone, email, in-app feedback, and supported Tier 2 staff with rental and part delivery cases', 'Utilized Amazon Web Services to aid Toyota Canada clients, providing information and technical support', 'Promoted to Email Support Specialist within two months, and started Tier 2 training within six months'],
   },
   
   {
@@ -26,7 +26,7 @@ export const experienceData: IExperience[] = [
     companyName: 'Canada Revenue Agency',
     companyImage: image1,
     date: 'Sep 2022 - Aug 2023 (1 year)',
-    points: ['Provided technical support and maintained IT infrastructure to ensure smooth operations', 'Collaborated with IT teams to escalate and resolve complex technical issues efficiently', 'Implemented monitoring tools and procedures to identify potential issues proactively and mitigate risks', 'Ensured system stability and reliability through continuous infrastructure maintenance', 'Facilitated communication between IT teams and end-users to ensure timely issue resolution'],
+    points: ['Granted special le/folder/software permissions to CRA employees as requested', 'Created/Deleted/Reactivated new and old employee accounts and updated their permissions', 'Collaborated with IT teams to troubleshoot and resolve complex issues, such as operating system glitches, hardware incompatibilities, software bugs, lack of permissions and much more', 'Managed all French ticket requests as well as translating emails and documents'],
   },
 
   {
@@ -34,7 +34,7 @@ export const experienceData: IExperience[] = [
     companyName: 'University of Windsory',
     companyImage: image2,
     date: 'May 2021 - Apr 2022 (1 year)',
-    points: ['Developed learning resources, including videos, graphics, websites, and animations', 'Designed visual learning elements for both student and faculty', 'Collaborated with departments and schools on multimedia projects', 'Enhanced the universitys multimedia presence through diverse contents', 'Supported faculty and students with custom course materials'],
+    points: ['Led multimedia projects including video and animation production, graphic design work, HTML and CSS website building, 3D modeling, game design and much more', 'Worked with various university departments and professors to create custom infographics, visually interesting presentations and engaging digital syllabi'],
   },
 
   {
@@ -42,6 +42,6 @@ export const experienceData: IExperience[] = [
     companyName: 'Subway',
     companyImage: image4,
     date: 'Jul 2017 - Sep 2021 (4+ year)',
-    points: ['Managed store operations including opening/closing, handeling cash, supervising and training team members, cleaning, food prep, serving, stocking and so much more.', 'Being bilingual, I would often take orders of the French speaking customers', 'After 4 years of working here, I was in the highest position of seniority which meant I was trusted with many leadership responsibilities.', 'Took it upon myself to create custom training documentation and learning ressources for the new hires I would train.', 'Learnt a lot about customer interactions and how to effectively deescalate tense situations.'],
+    points: ['Managed store operations including opening/closing, handling cash, supervising and training team members, cleaning, food prep, serving, stocking and so much more.', 'Being bilingual, I would often take orders of the French speaking customers', 'After 4 years of working here, I was in the highest position of seniority which meant I was trusted with many leadership responsibilities.', 'Took it upon myself to create custom training documentation and learning resources for the new hires I would train.', 'Learned a lot about customer interactions and how to effectively deescalate tense situations.'],
   },
 ];
